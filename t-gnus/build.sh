9ea8e697ffa1f8245de8d9a88fbc7150c81f38c4
kunishi <devnull@localhost>
2002-12-10 14:21:02 +0000

    new file.

---
+++ t-gnus/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. ../config.sh
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.jpl.org/elips/t-gnus-6.15/snapshots/t-gnus-6_15_8-05-quimby.tar.gz'
+WRKSRC=${WRKDIR}/t-gnus-6_15_8-05-quimby
+ELC_SHAREABLE=true
+
+. ../target.sh
+
+build_emacs () {
+    (cd ${WRKSRC}; ./configure --prefix=${BASEDIR} --with-emacs=${emacs})
+    (cd ${WRKSRC}; ${GMAKE})
+}
+
+install_emacs () {
+    (cd ${WRKSRC}; ${GMAKE} install-lisp install-etc install-info-ja)
+}
+
+init
+eval $1
