beb6a340d1549cc152fbc1a096fd0b9167b55aed
kunishi <devnull@localhost>
2002-12-11 10:56:31 +0000

    sync up with 1.4.1.

--- semantic/build.sh
+++ semantic/build.sh
@@ -4,8 +4,8 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://us.dl.sourceforge.net/cedet/semantic-1.4.tar.gz'
-WRKSRC=${WRKDIR}/semantic-1.4
+DISTFILES='http://us.dl.sourceforge.net/cedet/semantic-1.4.1.tar.gz'
+WRKSRC=${WRKDIR}/semantic-1.4.1
 ELC_SHAREABLE=true
 
 . ../target.sh
