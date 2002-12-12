505f3a2e6449e614ae0e7c97280eaeff396d8c3a
kunishi <devnull@localhost>
2002-12-12 00:51:20 +0000

    new port for docbookide.

---
+++ docbookide/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://nwalsh.com/emacs/docbookide/docbookide01.zip'
+WRKSRC=${WRKDIR}/docbookide
+ELC_SHAREABLE=true
+
+. ../target.sh
+
+build_emacs () {
+    (cd ${WRKSRC}; \
+	echo '(setq load-path (cons "." load-path))' > tmp.el; \
+	${emacs} ${EMACS_COMPILE_ARGS} -l tmp.el -f batch-byte-compile *.el; \
+	rm -f tmp.el)
+}
+
+install_emacs () {
+    mkdir -p ${BASEDIR}/share/emacs/site-lisp/docbookide
+    cp -p ${WRKSRC}/*.el ${BASEDIR}/share/emacs/site-lisp/docbookide
+    cp -p ${WRKSRC}/*.elc ${BASEDIR}/share/emacs/site-lisp/docbookide
+}
+
+init
+eval $1
