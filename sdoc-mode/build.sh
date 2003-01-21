8d09a0c28a01a56bbc663c74e7f3bf3d393ea663
kunishi <devnull@localhost>
2003-01-21 05:08:37 +0000

    new port for sdoc-mode.

---
+++ sdoc-mode/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.xmlsmartdoc.org/download/beta.zip'
+WRKSRC=${WRKDIR}
+ELC_SHAREABLE=true
+
+. ../target.sh
+
+patch_emacs () {
+	:
+}
+
+build_emacs () {
+	(cd ${WRKSRC}; jar xvf archive.jar etc)
+	(cd ${WRKSRC}; patch -p0 -N < ${PKG_TOPDIR}/patch/patch-emacs)
+	(cd ${WRKSRC}/etc; \
+	  ${emacs} ${EMACS_COMPILE_ARGS} -f batch-byte-compile *.el)
+}
+
+install_emacs () {
+	local sitelispdir=${BASEDIR}/share/emacs/site-lisp
+	cp -p ${WRKSRC}/etc/*.el[c] ${sitelispdir}
+}
+
+init
+eval $1
