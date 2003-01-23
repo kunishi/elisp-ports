618087894f7b61d6c43ec7e7b48be5c6aac20e67
kunishi <devnull@localhost>
2003-01-23 07:21:21 +0000

    sync up with 3.1.52.

--- mew/build.sh
+++ mew/build.sh
@@ -4,8 +4,8 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='ftp://ftp.mew.org/pub/Mew/beta/mew-3.1.51.tar.gz'
-WRKSRC=${WRKDIR}/mew-3.1.51
+DISTFILES='ftp://ftp.mew.org/pub/Mew/beta/mew-3.1.52.tar.gz'
+WRKSRC=${WRKDIR}/mew-3.1.52
 ELC_SHAREABLE=true
 EMACS_BUILD_TARGET=''
 EMACS_INSTALL_TARGET='install install-info install-jinfo install-etc'
