3ae6c3dcc565ff0948a6a8e1fa65de179b544c72
kunishi <devnull@localhost>
2003-02-13 04:47:40 +0000

    sync up with 1.91.1.

--- ecb/build.sh
+++ ecb/build.sh
@@ -4,14 +4,14 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://us.dl.sourceforge.net/ecb/ecb-1.90.tar.gz'
-WRKSRC=${WRKDIR}/ecb-1.90
+DISTFILES='http://us.dl.sourceforge.net/ecb/ecb-1.91.1.tar.gz'
+WRKSRC=${WRKDIR}/ecb-1.91.1
 ELC_SHAREABLE=true
 
 . ../target.sh
 
 build_emacs () {
-    (cd ${WRKSRC}; ${GMAKE} all qEMACS=${emacs} \
+    (cd ${WRKSRC}; ${GMAKE} all EMACS=${emacs} \
 	LOADPATH="${BASEDIR}/share/emacs/site-lisp/eieio ${BASEDIR}/share/emacs/site-lisp/semantic ${BASEDIR}/share/emacs/site-lisp/jde/lisp")
 }
 
@@ -19,7 +19,7 @@ install_emacs () {
     local sitelispdir=${BASEDIR}/share/emacs/site-lisp/ecb
     mkdir -p ${sitelispdir}
     cp -p ${WRKSRC}/*.el ${WRKSRC}/*.elc ${sitelispdir}
-    cp -p ${WRKSRC}/ecb.info ${BASEDIR}/info
+    cp -p ${WRKSRC}/info-help/ecb.info* ${BASEDIR}/info
     install-info ${BASEDIR}/info/ecb.info ${BASEDIR}/info/dir
 }
 
