53bb8a8844f5001294862ac012d7cfe64f3dca75
kunishi <devnull@localhost>
2003-05-31 16:19:18 +0000

    merge conflicts.

--- ecb/build.sh
+++ ecb/build.sh
@@ -4,8 +4,8 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://us.dl.sourceforge.net/ecb/ecb-1.92.tar.gz'
-WRKSRC="${WRKDIR}/ecb-1.92"
+DISTFILES='http://us.dl.sourceforge.net/ecb/ecb-1.92.1.tar.gz'
+WRKSRC=${WRKDIR}/ecb-1.92.1
 USE_EMACS=true
 ELC_SHAREABLE=true
 
