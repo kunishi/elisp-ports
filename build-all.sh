0b4dda34d5760976ffb5b1e8fce5da2cfc1d55eb
kunishi <devnull@localhost>
2003-06-01 14:25:07 +0000

    changed for new scheme.

--- build-all.sh
+++ build-all.sh
@@ -4,6 +4,7 @@ for pkg in `cat default-packages`
 do
     echo Installing $pkg
     (cd $pkg; \
+	env CONFIG_SH=${CONFIG_SH} ./build.sh build; \
 	env CONFIG_SH=${CONFIG_SH} sudo ./build.sh install; \
-	env CONFIG_SH=${CONFIG_SH} sudo ./build.sh clean)
+	env CONFIG_SH=${CONFIG_SH} ./build.sh clean)
 done
