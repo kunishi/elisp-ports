d8388cb6867d350ac79f8d3e5ab3a420d99c7c1e
kunishi <devnull@localhost>
2003-06-02 03:48:07 +0000

    new port for epojava.

---
+++ epojava/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/06/02 03:48:07 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://downloads.sourceforge.jp/epojava/3890/epojava.zip'
+WRKSRC="${WRKDIR}/epojava"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=true
+ELC_SHAREABLE=true
+
+. ../target.sh
+
+build_emacs () {
+    : write how to build package
+}
+
+install_emacs () {
+    : write how to install package
+    mkdir -p ${SITELISPDIR}/epojava
+    (cd ${WRKSRC}; cp -p *.el *.jar ${SITELISPDIR}/epojava)
+    mkdir -p ${BASEDIR}/share/doc/epojava
+    cp -rp ${WRKSRC}/doc ${BASEDIR}/share/doc/epojava
+}
+
+init
+eval $1
