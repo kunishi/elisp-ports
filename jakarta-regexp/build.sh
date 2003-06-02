e28005fe4d9bcb538be75332de8f6c82f32c211c
kunishi <devnull@localhost>
2003-06-02 03:30:05 +0000

    new port for Jakarta Regexp.

---
+++ jakarta-regexp/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/06/02 03:30:05 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.apache.org/dist/jakarta/regexp/jakarta-regexp-1.2.zip'
+WRKSRC="${WRKDIR}"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+    install_java jakarta-regexp-1.2 jakarta-regexp
+}
+
+init
+eval $1
