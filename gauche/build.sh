cf660abdd62a8c99bd8aa079ee4a53202da08567
kunishi <devnull@localhost>
2003-06-03 12:02:13 +0000

    sync up with Gauche 0.7.

--- gauche/build.sh
+++ gauche/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: build.sh,v 1.2 2003/06/01 12:34:25 kunishi Exp $
+# $Id: build.sh,v 1.3 2003/06/03 12:02:13 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://us.dl.sourceforge.net/gauche/Gauche-0.6.8.tgz'
-WRKSRC="${WRKDIR}/Gauche-0.6.8"
+DISTFILES='http://us.dl.sourceforge.net/gauche/Gauche-0.7.tgz'
+WRKSRC="${WRKDIR}/Gauche-0.7"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=false
 
