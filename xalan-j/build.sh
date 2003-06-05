c7b2d54613444fc6ba4e2d2aacc3a40c549165e7
kunishi <devnull@localhost>
2003-06-05 09:22:04 +0000

    sync up with Xalan-J 2.5.1.

--- xalan-j/build.sh
+++ xalan-j/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: build.sh,v 1.2 2003/06/02 03:10:48 kunishi Exp $
+# $Id: build.sh,v 1.3 2003/06/05 09:22:04 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://xml.apache.org/dist/xalan-j/xalan-j_2_5_0-bin.zip'
-WRKSRC="${WRKDIR}/xalan-j_2_5_0"
+DISTFILES='http://xml.apache.org/dist/xalan-j/xalan-j_2_5_1-bin.zip'
+WRKSRC="${WRKDIR}/xalan-j_2_5_1"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=false
 
@@ -18,7 +18,7 @@ build_target () {
 
 install_target () {
     : install targets for non-Emacsen ports
-    install_java xalan-j-2.5.0 xalan-j
+    install_java xalan-j-2.5.1 xalan-j
 }
 
 init
