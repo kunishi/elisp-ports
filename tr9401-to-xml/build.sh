e64d07053fc44fd6501394c5d70b3cb73d5801ca
kunishi <devnull@localhost>
2003-06-11 09:45:17 +0000

    new port for a conversion script from TR9401 catalog to XML catalog.

---
+++ tr9401-to-xml/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/06/11 09:45:17 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.chorusroom.org/~kunishi/distfiles/tr9401-to-xml.pl'
+WRKSRC="${WRKDIR}"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+    chmod 755 ${WRKSRC}/tr9401-to-xml.pl
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+    cp ${WRKSRC}/tr9401-to-xml.pl ${BASEDIR}/bin
+}
+
+init
+eval $1
