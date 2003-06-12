5037e0405bc234022da95c347ce351d8d1e0f5cb
kunishi <devnull@localhost>
2003-06-12 04:26:32 +0000

    new port for DocBook V4.2.

---
+++ docbook42/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/06/12 04:26:32 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.oasis-open.org/docbook/xml/4.2/docbook-xml-4.2.zip'
+WRKSRC="${WRKDIR}"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+    install_xml docbook-4.2
+}
+
+init
+eval $1
