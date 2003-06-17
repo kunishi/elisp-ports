0d4e497056997b4d911f04987c10837c33257992
kunishi <devnull@localhost>
2003-06-17 02:05:39 +0000

    use xmlcatalog.

--- docbook40/build.sh
+++ docbook40/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: build.sh,v 1.1 2003/06/12 02:49:40 kunishi Exp $
+# $Id: build.sh,v 1.2 2003/06/17 02:05:39 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
@@ -12,6 +12,7 @@ USE_EMACS=false
 
 DIST_BASEURI="http://www.oasis-open.org/docbook/xml/4.0/"
 LOCAL_BASEURI="file://${BASEDIR}/share/xml/docbook-4.0/"
+XML_CATALOG="${BASEDIR}/share/xml/xmlcatalog"
 
 . ../target.sh
 
@@ -24,6 +25,16 @@ build_target () {
 install_target () {
     : install targets for non-Emacsen ports
     install_xml docbook-4.0
+    if [ -f ${BASEDIR}/bin/xmlcatalog ]; then
+	xmlcatalog --noout --add public '-//USA-DOD//DTD Table Model 951010//EN' docbook-4.0/calstblx.mod ${XML_CATALOG}
+	xmlcatalog --noout --add 'public' '-//OASIS//ENTITIES DocBook XML Character Entities V4.0//EN' 'docbook-4.0/dbcentx.mod' ${XML_CATALOG}
+	xmlcatalog --noout --add 'public' '-//OASIS//ENTITIES DocBook Additional General Entities V4.0//EN' 'docbook-4.0/dbgenent.ent' ${XML_CATALOG}
+	xmlcatalog --noout --add 'public' '-//OASIS//ELEMENTS DocBook XML Document Hierarchy V4.0//EN' 'docbook-4.0/dbhierx.mod' ${XML_CATALOG}
+	xmlcatalog --noout --add public '-//OASIS//ENTITIES DocBook XML Notations V4.0//EN' docbook-4.0/dbnotnx.mod ${XML_CATALOG}
+	xmlcatalog --noout --add public '-//OASIS//ELEMENTS DocBook XML Information Pool V4.0//EN' docbook-4.0/dbpoolx.mod ${XML_CATALOG}
+	xmlcatalog --noout --add public '-//OASIS//DTD DocBook XML V4.0//EN' docbook-4.0/docbookx.dtd ${XML_CATALOG}
+	xmlcatalog --noout --add public '-//OASIS//DTD XML Exchange Table Model 19990315//EN' docbook-4.0/soextblx.dtd ${XML_CATALOG}
+    fi
 }
 
 init
