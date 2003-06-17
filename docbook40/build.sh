1af5897c3b51617eb9605c3aa3a3e936bef82cdb
kunishi <devnull@localhost>
2003-06-17 06:51:20 +0000

    use xmlcatmgr.

--- docbook40/build.sh
+++ docbook40/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: build.sh,v 1.2 2003/06/17 02:05:39 kunishi Exp $
+# $Id: build.sh,v 1.3 2003/06/17 06:51:20 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
@@ -12,7 +12,6 @@ USE_EMACS=false
 
 DIST_BASEURI="http://www.oasis-open.org/docbook/xml/4.0/"
 LOCAL_BASEURI="file://${BASEDIR}/share/xml/docbook-4.0/"
-XML_CATALOG="${BASEDIR}/share/xml/xmlcatalog"
 
 . ../target.sh
 
@@ -25,15 +24,15 @@ build_target () {
 install_target () {
     : install targets for non-Emacsen ports
     install_xml docbook-4.0
-    if [ -f ${BASEDIR}/bin/xmlcatalog ]; then
-	xmlcatalog --noout --add public '-//USA-DOD//DTD Table Model 951010//EN' docbook-4.0/calstblx.mod ${XML_CATALOG}
-	xmlcatalog --noout --add 'public' '-//OASIS//ENTITIES DocBook XML Character Entities V4.0//EN' 'docbook-4.0/dbcentx.mod' ${XML_CATALOG}
-	xmlcatalog --noout --add 'public' '-//OASIS//ENTITIES DocBook Additional General Entities V4.0//EN' 'docbook-4.0/dbgenent.ent' ${XML_CATALOG}
-	xmlcatalog --noout --add 'public' '-//OASIS//ELEMENTS DocBook XML Document Hierarchy V4.0//EN' 'docbook-4.0/dbhierx.mod' ${XML_CATALOG}
-	xmlcatalog --noout --add public '-//OASIS//ENTITIES DocBook XML Notations V4.0//EN' docbook-4.0/dbnotnx.mod ${XML_CATALOG}
-	xmlcatalog --noout --add public '-//OASIS//ELEMENTS DocBook XML Information Pool V4.0//EN' docbook-4.0/dbpoolx.mod ${XML_CATALOG}
-	xmlcatalog --noout --add public '-//OASIS//DTD DocBook XML V4.0//EN' docbook-4.0/docbookx.dtd ${XML_CATALOG}
-	xmlcatalog --noout --add public '-//OASIS//DTD XML Exchange Table Model 19990315//EN' docbook-4.0/soextblx.dtd ${XML_CATALOG}
+    if [ -f ${BASEDIR}/bin/xmlcatmgr ]; then
+	xmlcatmgr add public '-//USA-DOD//DTD Table Model 951010//EN' docbook-4.0/calstblx.mod
+	xmlcatmgr add public '-//OASIS//ENTITIES DocBook XML Character Entities V4.0//EN' docbook-4.0/dbcentx.mod
+	xmlcatmgr add public '-//OASIS//ENTITIES DocBook Additional General Entities V4.0//EN' docbook-4.0/dbgenent.ent
+	xmlcatmgr add public '-//OASIS//ELEMENTS DocBook XML Document Hierarchy V4.0//EN' docbook-4.0/dbhierx.mod
+	xmlcatmgr add public '-//OASIS//ENTITIES DocBook XML Notations V4.0//EN' docbook-4.0/dbnotnx.mod
+	xmlcatmgr add public '-//OASIS//ELEMENTS DocBook XML Information Pool V4.0//EN' docbook-4.0/dbpoolx.mod
+	xmlcatmgr add public '-//OASIS//DTD DocBook XML V4.0//EN' docbook-4.0/docbookx.dtd
+	xmlcatmgr add public '-//OASIS//DTD XML Exchange Table Model 19990315//EN' docbook-4.0/soextblx.dtd
     fi
 }
 
