ef3fe28619ce6f6819621c50bff695cfc148dac0
kunishi <devnull@localhost>
2003-06-22 11:23:02 +0000

    new port for SAX2.

---
+++ sax2/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/06/22 11:23:02 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://us.dl.sourceforge.net/sax/sax2r2.jar'
+WRKSRC="${WRKDIR}/sax2.0.1"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+    (cd ${WRKDIR}; jar xvf sax2r2.jar)
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+    install_java sax-2.0.1 sax2
+}
+
+init
+eval $1
