732195093e0b00151f6a7426718356a8cfc966d5
kunishi <devnull@localhost>
2003-06-22 11:06:20 +0000

    new port for xml-writer.

---
+++ xml-writer/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/06/22 11:06:20 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.megginson.com/Software/xml-writer-0.2.zip'
+WRKSRC="${WRKDIR}/xml-writer-0.2"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+    install_java xml-writer-0.2 xml-writer
+}
+
+init
+eval $1
