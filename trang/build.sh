df0d24f55e0f9680d8a5a186d685a53c66095aa7
kunishi <devnull@localhost>
2003-06-23 01:34:33 +0000

    sync up with Trang 20030619.

--- trang/build.sh
+++ trang/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: build.sh,v 1.1 2003/06/12 05:06:43 kunishi Exp $
+# $Id: build.sh,v 1.2 2003/06/23 01:34:33 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://thaiopensource.com/relaxng/trang-bin.zip'
-WRKSRC="${WRKDIR}"
+DISTFILES='http://www.thaiopensource.com/download/trang-20030619.zip'
+WRKSRC="${WRKDIR}/trang-20030619"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=false
 
@@ -18,7 +18,7 @@ build_target () {
 
 install_target () {
     : install targets for non-Emacsen ports
-    install_java trang-20030227 trang
+    install_java trang-20030619 trang
 }
 
 init
