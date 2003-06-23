1eea921f4195eea87b23086fdd8b74149796a64f
kunishi <devnull@localhost>
2003-06-23 01:36:07 +0000

    sync up with DTDInst 20030619.

--- dtdinst/build.sh
+++ dtdinst/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: build.sh,v 1.1 2003/06/12 05:10:56 kunishi Exp $
+# $Id: build.sh,v 1.2 2003/06/23 01:36:07 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://thaiopensource.com/relaxng/dtdinst/dtdinst.jar'
-WRKSRC="${WRKDIR}"
+DISTFILES='http://www.thaiopensource.com/download/dtdinst-20030619.zip'
+WRKSRC="${WRKDIR}/dtdinst-20030619"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=false
 
@@ -18,7 +18,7 @@ build_target () {
 
 install_target () {
     : install targets for non-Emacsen ports
-    install_java dtdinst-20030122 dtdinst
+    install_java dtdinst-20030619 dtdinst
 }
 
 init
