afe21998430a8a3591ccf28534358333f8343405
kunishi <devnull@localhost>
2003-06-23 01:32:59 +0000

    sync up with Jing 20030619.

--- jing/build.sh
+++ jing/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: build.sh,v 1.1 2003/06/12 05:02:29 kunishi Exp $
+# $Id: build.sh,v 1.2 2003/06/23 01:32:59 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://thaiopensource.com/relaxng/jing-bin.zip'
-WRKSRC="${WRKDIR}"
+DISTFILES='http://www.thaiopensource.com/download/jing-20030619.zip'
+WRKSRC="${WRKDIR}/jing-20030619"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=false
 
@@ -18,7 +18,7 @@ build_target () {
 
 install_target () {
     : install targets for non-Emacsen ports
-    install_java jing-20030226 jing
+    install_java jing-20030619 jing
 }
 
 init
