7fcfcf8e5718f09d5b50fd8c4f811953534e6541
kunishi <devnull@localhost>
2003-06-23 08:10:39 +0000

    sync up with docbook-xsl 1.61.3.

--- docbook-xsl/build.sh
+++ docbook-xsl/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: build.sh,v 1.1 2003/06/12 06:56:43 kunishi Exp $
+# $Id: build.sh,v 1.2 2003/06/23 08:10:39 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://us.dl.sourceforge.net/docbook/docbook-xsl-1.61.2.tar.gz'
-WRKSRC="${WRKDIR}/docbook-xsl-1.61.2"
+DISTFILES='http://us.dl.sourceforge.net/docbook/docbook-xsl-1.61.3.tar.gz'
+WRKSRC="${WRKDIR}/docbook-xsl-1.61.3"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=false
 
