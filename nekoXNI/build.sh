088171bb06e330ab1fbd320088469b1801636f44
kunishi <devnull@localhost>
2003-06-26 05:05:17 +0000

    new port for nekoXNI.

---
+++ nekoXNI/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/06/26 05:05:17 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.apache.org/~andyc/neko/nekoxni-latest.tar.gz'
+WRKSRC="${WRKDIR}/nekoxni-2003.06.25"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+    install_java nekoxni-2003.06.25 nekoxni
+}
+
+init
+eval $1
