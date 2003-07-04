e062fc0781a11deefd1d7a2a51c2da011a03382d
kunishi <devnull@localhost>
2003-07-04 07:44:39 +0000

    new port for PXP.  Not finished.

---
+++ ocaml-pxp/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/07/04 07:44:39 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.ocaml-programming.de/packages/pxp-1.1.94.tar.gz'
+WRKSRC="${WRKDIR}/pxp-1.1.94"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+    (cd ${WRKSRC}; ./configure && ${GMAKE} all opt)
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+}
+
+init
+eval $1
