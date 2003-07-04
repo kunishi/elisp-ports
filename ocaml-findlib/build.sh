c9be4e007b22f0a3d6ea9c7a3aafcfca26a4df73
kunishi <devnull@localhost>
2003-07-04 05:51:41 +0000

    new port for OCaml findlib.

---
+++ ocaml-findlib/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# $Id: build.sh,v 1.1 2003/07/04 05:51:41 kunishi Exp $
+
+. ../${CONFIG_SH:-config.sh}
+
+PKG_TOPDIR=`pwd`
+
+DISTFILES='http://www.ocaml-programming.de/packages/findlib-0.8.1.tar.gz'
+WRKSRC="${WRKDIR}/findlib-0.8.1"
+#PATCHFILES='If you have some official patch, write them'
+USE_EMACS=false
+
+. ../target.sh
+
+build_target () {
+    : targets for non-Emacsen ports
+    (cd ${WRKSRC}; ./configure && ${GMAKE} all opt)
+}
+
+install_target () {
+    : install targets for non-Emacsen ports
+    (cd ${WRKSRC}; ${GMAKE} install)
+}
+
+init
+eval $1
