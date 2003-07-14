201a7cbf4624fcaf8691fc848d19a06729a67644
kunishi <devnull@localhost>
2003-07-14 03:45:50 +0000

    sync up with 1.0 RC3.

--- relaxer/build.sh
+++ relaxer/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $Id: build.sh,v 1.2 2003/06/16 23:18:04 kunishi Exp $
+# $Id: build.sh,v 1.3 2003/07/14 03:45:50 kunishi Exp $
 
 . ../${CONFIG_SH:-config.sh}
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://www.relaxer.org/download/setup.zip'
+DISTFILES='http://www.relaxer.org/download/beta.zip'
 WRKSRC="${WRKDIR}"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=false
@@ -22,7 +22,7 @@ build_target () {
 
 install_target () {
     : install targets for non-Emacsen ports
-    java -jar ${DISTDIR}/setup.zip setup
+    java -jar ${DISTDIR}/beta.zip setup
     chmod 755 ${BASEDIR}/bin/relaxer
 }
 
