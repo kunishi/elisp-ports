0c73748806bbc0c06c4d273a96cb2880f2baf2ce
kunishi <devnull@localhost>
2003-07-29 23:40:20 +0000

    fix for Cygwin.

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+case `uname` in
+CYGWIN*)
+	EXEC_SHELL=bash
+	;;
+*)
+	SUDO=sudo
+	EXEC_SHELL=/bin/sh
+esac
+
 for pkg in `cat $1`
 do
     echo Installing $pkg
     (cd $pkg; \
-	env CONFIG_SH=${CONFIG_SH} ./build.sh build; \
-	env CONFIG_SH=${CONFIG_SH} sudo ./build.sh install; \
-	env CONFIG_SH=${CONFIG_SH} ./build.sh clean)
+	env CONFIG_SH=${CONFIG_SH} $EXEC_SHELL ./build.sh build; \
+	env CONFIG_SH=${CONFIG_SH} $SUDO $EXEC_SHELL ./build.sh install; \
+	env CONFIG_SH=${CONFIG_SH} $EXEC_SHELL ./build.sh clean)
 done
