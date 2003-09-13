17fb7deb73a0b5e2e82876417da6aca1ce37b615
kunishi <devnull@localhost>
2003-09-13 00:10:16 +0000

    sync up with nxml-mode 20030912.

--- nxml-mode/build.sh
+++ nxml-mode/build.sh
@@ -5,8 +5,8 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://www.thaiopensource.com/download/nxml-mode-20030906.tar.gz'
-WRKSRC="${WRKDIR}/nxml-mode-20030906"
+DISTFILES='http://www.thaiopensource.com/download/nxml-mode-20030912.tar.gz'
+WRKSRC="${WRKDIR}/nxml-mode-20030912"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=true
 ELC_SHAREABLE=true
