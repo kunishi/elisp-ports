0ce1658f5e1ff005fb4f134879f2f03189dd8131
kunishi <devnull@localhost>
2003-09-25 08:17:27 +0000

    support RSS 1.0.

--- nxml-mode/dotemacs.el
+++ nxml-mode/dotemacs.el
@@ -1,10 +1,15 @@
 (load-safe "rng-auto")
 (setq auto-mode-alist
-      (cons '("\\.\\(xml\\|xsl\\|rng\\|rxm\\|rxg\\|xhtml\\)\\'" . nxml-mode)
-	    auto-mode-alist))
+      (cons
+       '("\\.\\(xml\\|xsl\\|rng\\|rxm\\|rxg\\|rss\\|xhtml\\)\\'" . nxml-mode)
+       auto-mode-alist))
 (if (file-exists-p "/usr/local/share/xml/relaxDtd/relaxCore.rnc")
     (add-to-list 'rng-auto-file-name-alist-default
 		 '(".*\\.rxm\\'" "/usr/local/share/xml/relaxDtd/relaxCore.rnc")))
 (if (file-exists-p "/usr/local/share/xml/relaxDtd/relaxNamespace.rnc")
     (add-to-list 'rng-auto-file-name-alist-default
 		 '(".*\\.rxg\\'" "/usr/local/share/xml/relaxDtd/relaxNamespace.rnc")))
+
+(if (file-exists-p "/usr/local/share/xml/rng4rss10/rss-1_0.rnc")
+    (add-to-list 'rng-auto-file-name-alist-default
+		 '(".*\\.rss\\'" "/usr/local/share/xml/rng4rss10/rss-1_0.rnc")))
