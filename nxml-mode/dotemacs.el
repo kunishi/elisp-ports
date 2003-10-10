23d3c78c538fd1950c67ffac6ff96907066e7bd9
kunishi <devnull@localhost>
2003-10-10 07:54:31 +0000

    settings for Ant XML.

--- nxml-mode/dotemacs.el
+++ nxml-mode/dotemacs.el
@@ -13,3 +13,9 @@
 (if (file-exists-p "/usr/local/share/xml/rng4rss10/rss-1_0.rnc")
     (add-to-list 'rng-auto-file-name-alist-default
 		 '(".*\\.rss\\'" "/usr/local/share/xml/rng4rss10/rss-1_0.rnc")))
+(if (file-exists-p "/usr/local/share/xml/ant-dtd/project.rnc")
+    (setq rng-auto-element-alist-default
+	  (cons
+	   '((t t "project")
+	     . "/usr/local/share/xml/ant-dtd/project.rnc")
+	   rng-auto-element-alist-default)))
