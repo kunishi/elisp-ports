eb5766b4d6c5e69d0a19d5ce6bf87a80dc6bbb4b
kunishi <devnull@localhost>
2003-10-31 07:00:14 +0000

    chase up with nxml-mode 20031031.

--- nxml-mode/build.sh
+++ nxml-mode/build.sh
@@ -6,8 +6,8 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://www.thaiopensource.com/download/nxml-mode-20031018.tar.gz'
-WRKSRC="${WRKDIR}/nxml-mode-20031018"
+DISTFILES='http://www.thaiopensource.com/download/nxml-mode-20031031.tar.gz'
+WRKSRC="${WRKDIR}/nxml-mode-20031031"
 #PATCHFILES='If you have some official patch, write them'
 USE_EMACS=true
 ELC_SHAREABLE=true
