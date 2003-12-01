def6a433c0c14ff923c31812b09d7813837826a6
kunishi <devnull@localhost>
2003-12-01 06:06:33 +0000

    Win32 batch files.

---
+++ relaxer/build.bat
@@ -0,0 +1,3 @@
+wget http://www.relaxer.org/download/beta.zip
+java -jar beta.zip setup
+del beta.zip
---
+++ smartdoc/build.bat
@@ -0,0 +1,3 @@
+wget http://www.xmlsmartdoc.org/download/beta.zip
+java -jar beta.zip setup
+del beta.zip
