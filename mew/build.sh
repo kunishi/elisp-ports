ca61ae31ad03bb78c11253e7f96b3112d0d33107
kunishi <devnull@localhost>
2004-03-08 09:06:21 +0000

    sync up with Mew 4.0.64.

--- mew/build.sh
+++ mew/build.sh
@@ -5,8 +5,8 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='ftp://ftp.mew.org/pub/Mew/alpha/mew-4.0.63.tar.gz'
-WRKSRC="${WRKDIR}/mew-4.0.63"
+DISTFILES='ftp://ftp.mew.org/pub/Mew/alpha/mew-4.0.64.tar.gz'
+WRKSRC="${WRKDIR}/mew-4.0.64"
 USE_EMACS=true
 ELC_SHAREABLE=true
 EMACS_BUILD_TARGET=''
