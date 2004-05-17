05511978dfffdd987104869ef46225a63a42652f
www <devnull@localhost>
2004-05-17 21:32:05 +0000

    fixed a bug.

--- carbon-font/dotemacs.el
+++ carbon-font/dotemacs.el
@@ -7,6 +7,5 @@
     "japanese-jisx0208:-apple-ヒラギノ角ゴ pro w3-medium-r-normal--16-160-75-75-m-160-jisx0208.1983-sjis,"
     "katakana-jisx0201:-apple-ヒラギノ角ゴ pro w3-medium-r-normal-*-160-*-jisx0201*-*,"
     "korean-ksc5601:-apple-applegothic-medium-r-normal--16-160-75-75-m-160-ksc5601.1989-0,"
-    "ascii:-etl-fixed--16-*-iso8859-1,"
     "latin-iso8859-1:-etl-fixed--16-*-iso8859-1"))
   (set-default-font "fontset-hiraginokaku16"))
