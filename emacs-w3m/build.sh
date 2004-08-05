c3f76f09844aedfcfbaa18f55d805889ac11b866
kunishi <devnull@localhost>
2004-08-05 11:01:37 +0000

    sync up with 1.4.2.

--- emacs-w3m/build.sh
+++ emacs-w3m/build.sh
@@ -5,8 +5,8 @@
 
 PKG_TOPDIR=`pwd`
 
-DISTFILES='http://emacs-w3m.namazu.org/emacs-w3m-1.4.tar.gz'
-WRKSRC="${WRKDIR}/emacs-w3m-1.4"
+DISTFILES='http://emacs-w3m.namazu.org/emacs-w3m-1.4.2.tar.gz'
+WRKSRC="${WRKDIR}/emacs-w3m-1.4.2"
 USE_EMACS=true
 ELC_SHAREABLE=true
 
