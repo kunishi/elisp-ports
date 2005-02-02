55fe92a7345bd9ead76d6302be8b3308cf574838
www <devnull@localhost>
2005-02-02 01:59:41 +0000

    use xdvi rather than Mxdvi (open).

--- yatex/dotemacs.el
+++ yatex/dotemacs.el
@@ -3,10 +3,11 @@
   (add-to-list 'auto-mode-alist (cons "\\.latex\\'" 'yatex-mode))
   (add-to-list 'auto-mode-alist (cons "\\.ltx\\'" 'yatex-mode))
   (setq tex-command "platex")
-  (cond ((eq system-type 'darwin)
-	 (setq dvi2-command "open"))
-	(t
-	 (setq dvi2-command "xdvi")))
+;;   (cond ((eq system-type 'darwin)
+;; 	 (setq dvi2-command "open"))
+;; 	(t
+;; 	 (setq dvi2-command "xdvi")))
+  (setq dvi2-command "xdvi")
   (setq NTT-jTeX nil)
   (setq dviprint-command-format "dvips %f %t %s | lpr")
   (setq dviprint-from-format "-p %b")
