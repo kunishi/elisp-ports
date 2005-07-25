2a6dccd7f2cfc3d8cddda147b181f039bd72c78d
www <devnull@localhost>
2005-07-25 01:09:47 +0000

    add module for substituting jisx0208 Extention.

--- dotemacs.el
+++ dotemacs.el
@@ -11,6 +11,9 @@
 ;; (and (eq window-system 'mac)
 ;;      (load-safe "~/.emacs.d/init.d/utf-8m-init.el"))
 
+;; subst-jisx0208ex
+(load-safe "~/.emacs.d/init.d/subst-jisx0208ex-init.el"))
+
 ;; apel
 (load-safe "~/.emacs.d/init.d/apel-init.el")
 
